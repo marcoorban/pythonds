f9572e38f7d2f7d3f255de48000a2654adad8e93
Marco Orban <marco.orban@gmail.com>
2021-09-24 12:51:04 +0800

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,21 @@
+from graphs import Graph, Vertex
+from basic import Queue
+
+def bfs(G, S):
+    # Mark S as explored, all other vertices are unexplored
+    start = G.getVertex(S)
+    start.setExplored()
+    # create a queue and initialize it with S
+    Q = Queue()
+    Q.enqueue(start)
+    while not Q.isEmpty():
+        # remove the vertex from the front of Q and call it V
+        V = Q.dequeue()
+        # for each edge (V,W) in V's adjacency, if W is unexplored, mark it as explored and add it to the queue
+        for neighbor in V.getConnections():
+            if not neighbor.isExplored():
+                neighbor.isExplored()
+                Q.enqueue(neighbor)
+        
+        
+    
---
+++ bfstest.py
@@ -0,0 +1,25 @@
+from bfs import bfs
+from graphs import Graph, Vertex
+from basic import Queue
+
+G = Graph()
+V = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q']
+for vertex in V:
+    G.addVertex(vertex)
+print(G.getVertices())
+G.addEdge('a', 'b')
+G.addEdge('a', 'c')
+G.addEdge('a', 'd')
+G.addEdge('b', 'e')
+G.addEdge('b', 'f')
+G.addEdge('b', 'g')
+G.addEdge('h', 'i')
+G.addEdge('d', 'j')
+G.addEdge('e', 'k')
+G.addEdge('f', 'm')
+G.addEdge('k', 'l')
+G.addEdge('l', 'n')
+G.addEdge('o', 'p')
+G.addEdge('o', 'q')
+
+bfs(G, 'a')
